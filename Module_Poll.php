96af90176cdeba089af4e4fa3f2f759abcfd4017
gizmore <gizmore@wechall.net>
2022-10-04 11:48:07 +0200

    'KC'

--- Module_Poll.php
+++ Module_Poll.php
@@ -2,17 +2,49 @@
 namespace GDO\Poll;
 
 use GDO\Core\GDO_Module;
+use GDO\Core\GDT_Checkbox;
+use GDO\User\GDT_Level;
 
+/**
+ * Polls working in CLI and Web.
+ * 
+ * @author gizmore
+ * @version 7.0.1
+ */
 final class Module_Poll extends GDO_Module
 {
 	public int $priority = 40;
 	
 	public function getClasses() : array
 	{
-		return array(
-			"GDO\\Poll\GDO_Poll",
-			"GDO\\Poll\GDO_Choice",
-			"GDO\\Poll\GDO_Answer",
-		);
+		return [
+			GDO_Poll::class,
+			GDO_Choice::class,
+			GDO_Answer::class,
+		];
 	}
+	
+	############
+	### Init ###
+	############
+	public function onLoadLanguage(): void
+	{
+		$this->loadLanguage('lang/poll');
+	}
+	
+	##############
+	### Config ###
+	##############
+	public function getConfig(): array
+	{
+		return [
+			GDT_Checkbox::make('hook_sidebar')->initial('1'),
+			GDT_Checkbox::make('guest_votes')->initial('1'),
+			GDT_Level::make('level_per_poll')->initial('100'),
+		];
+	}
+	public function cfgSidebar(): bool { return $this->getConfigValue('hook_sidebar'); }
+	public function cfgGuestVotes(): bool { return $this->getConfigValue('guest_votes'); }
+	public function cfgLevelPerPoll(): int { return $this->getConfigValue('level_per_poll'); }
+	
 }
---
+++ lang/poll_en.php
@@ -0,0 +1,5 @@
+<?php
+namespace GDO\Poll\lang;
+return [
+	
+];
